ac3e4d36ac7e9e72cb0b997ee494b81553058bcf
alan-santos-helloworld2020 <alanprotesto@gmail.com>
2020-09-05 01:10:46 -0300

    alteração de css

--- public/js/script.js
+++ public/js/script.js
@@ -1,56 +1,6 @@
-
-let navegador = {
-
-
-    template:`
-    <nav>
-        <div class="nav-wrapper green">
-            <a href="/" class="brand-logo center">VueJS</a>
-            <ul id="nav-mobile" class="left hide-on-med-and-down">
-                <li><a href="/produtos">Produtos</a></li>
-                <li><a href="/cadastro">Cadastro</a></li>
-                <li><a href="/sobre">Sobre</a></li>
-            </ul>
-        </div>
-    </nav>
-
-    `
-}
-
-let rodape = {
-
-    template:`
-    <footer class="page-footer green">
-    <p class="center"> © 2020 Onload-Tecnologia</p>
-    </footer>
-    
-    `
-
-}
-
-
-
-let root = new Vue({
-
-    el: "#root",
-    data: {
-        nome: ""
-
-    },
-    created: () => {
-
-
-
-    },
-    components: {
-        'navegador': navegador,
-        'rodape': rodape
-    },
-    methods: {
-        saudar: () => {
-
-            alert(`Hello: ${this.nome}`);
-        }
-
-    }
+document.addEventListener('DOMContentLoaded', function () {
+    var sidenav = document.querySelectorAll('.sidenav');
+    var instances = M.Sidenav.init(sidenav,{
+        edge:"left"
+    });
 });
